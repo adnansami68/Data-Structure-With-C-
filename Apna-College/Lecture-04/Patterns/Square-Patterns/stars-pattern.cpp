0cbad91f122ce24b619116aa3d9d539431591837
Md. Adnan Sami <a.m.adnansami143@gmail.com>
2026-01-24 13:19:22 +0600

    Add square star pattern program in C++

---
+++ Apna-College/Lecture-04/Patterns/Square-Patterns/stars-pattern.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=4;
+
+    for(int i=0;i<n;i++){ //outer loop
+        for(int j=0;j<n;j++){ //inner loop
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
