09ef0f9ba80710fbd99cef8ec4bd11d9d6a33dd1
Md. Adnan Sami <a.m.adnansami143@gmail.com>
2026-01-24 12:53:52 +0600

    Add number-patterns.cpp for printing square patterns
    Implement a number pattern printing program.

---
+++ Apna-College/Lecture-04/Patterns/Square-Patterns/number-patterns.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=4;
+
+    for(int i=1;i<=n;i++){ //Outer Loop
+        for(int j=1;j<=n;j++){ //Inner Loop
+            //cout<<j; //printing loop
+            cout<<j<<" "; //for space
+        }
+        cout<<endl;
+    }
+}
