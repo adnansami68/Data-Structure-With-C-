40214c9ae25e36df0cf95f2d35a093ad5e99efa3
Md. Adnan Sami <a.m.adnansami143@gmail.com>
2026-03-31 21:10:17 +0600

    Add User Input for Ackermann Function

---
+++ CSE-221-and-222/Lab-Practice/01-04-2026/Ackermann_Function/User_Input.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int ackermann(int m, int n) {
+    if (m == 0) {
+        return n + 1;
+    } 
+    else if (m != 0 && n == 0) {
+        return ackermann(m - 1, 1);
+    } 
+    else if (m != 0 && n != 0) {
+        return ackermann(m - 1, ackermann(m, n - 1));
+    }
+    return 0;
+}
+
+int main() {
+    int m, n;
+    
+    cout << "Enter the value of m: ";
+    cin >> m;
+    
+    cout << "Enter the value of n: ";
+    cin >> n;
+    
+    int ans = ackermann(m, n);
+    
+    cout << "Result: " << ans << endl;
+    
+    return 0;
+}
